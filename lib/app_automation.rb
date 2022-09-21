a612f4b5e478db697b128e1f69f930f7778b927f
Blair Replogle <blair.replogle@gmail.com>
2022-09-20 18:14:53 -0600

    [0003] Add Require Statements

--- lib/app_automation.rb
+++ lib/app_automation.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 require 'app_automation/appium'
+require 'app_automation/emulator'
 require 'app_automation/network'
+require 'app_automation/simulator'
 require 'app_automation/version'
 
 module AppAutomation
